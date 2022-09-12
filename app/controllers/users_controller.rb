bca29630fd36f3aca0bec1a71e5a07a1f7f08361
Candice Smith MacKenzie <Mahony8@aol.com>
2022-09-11 20:26:22 -0400

    Ctreated signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode_token({ user_id: @user.id})
+      render json:{ user: @user, token: @token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :creature_type, :age, :password_digest)
+      params.require(:user).permit(:username, :creature_type, :age, :password)
     end
 end
