0d58c6d5bfb1e46baf759b77ffddde2849b88f1d
Candice Smith MacKenzie <Mahony8@aol.com>
2022-09-12 16:06:40 -0400

    Worked on Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,14 @@ class SessionsController < ApplicationController
     render json: current_user
   end
 
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       @token = encode_token({ user_id: @user.id})
+      render json:{ user: @user, token: @token }, status: :ok
+    else
+      render json: { errors: ["Username and Password must match"] }, status: :unprocessable_entity
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   
   get "/get-current-user" => "sessions#get_current_user"
+  post "/login" => "sessions#login"
   # Defines the root path route ("/")
   # root "articles#index"
 end
